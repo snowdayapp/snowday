f1f850764b92b08cf6abc38dce930e20669a5774
Ksisky <kevindsisk@gmail.com>
2016-04-19 13:34:47 -0400

    Added a few files and zipcodes.

--- snowday.py
+++ snowday.py
@@ -1,4 +1,6 @@
+#Main application file.
 from zipcodecheck import zipcodecheck
+import dictionaries
 def main():
 	print "Welcome to SnowDay, please type in your zipcode."
 	zipcode = int(raw_input('> '))
--- zipcodecheck.py
+++ zipcodecheck.py
@@ -1,3 +1,4 @@
+#Seperates the location of zipcodes from main file.
 from time import sleep
 
 def zipcodecheck(zipcode) :
@@ -21,12 +22,25 @@ def zipcodecheck(zipcode) :
 		url = 'pa'
 	elif zipcode in range(19701, 19980):
 		url = 'de'
+	elif zipcode in range(20001, 20100) or range(20201, 20600):
+		url = 'dc'
+	elif zipcode in range(20101, 20200) or range(22001, 24659):
+		url = 'va'
+	elif zipcode in range(20601, 21931):
+		url = 'md'
+	elif zipcode in range(24701, 26887):
+		url = 'wv'
+	elif zipcode in range(27006, 28910):
+		url = 'nc'
+	elif zipcode in range(29001, 29946):
+		url = 'sc'
+	elif zipcode in range(30001,)
 	else:
 		print "Invalid Zipcode, exiting in\n"
 		countdown = 3
 		for number1 in range(0, 4):
 			sleep(1)
-			print countdown
+			print countdown, "\n"
 			countdown -= 1
 		exit()
 	return url
